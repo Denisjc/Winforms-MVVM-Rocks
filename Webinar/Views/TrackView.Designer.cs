29f175fb48b6bc85e81f5629286ab00fc1968fc1
Denis Cilliers <dxcilliers@irishprisons.ie>
2024-10-25 15:36:19 +0100

    Initial Code from Github

---
+++ Webinar/Views/TrackView.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Webinar.Views
+{
+    partial class TrackView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.txtEditId = new DevExpress.XtraEditors.TextEdit();
+			this.txtEditName = new DevExpress.XtraEditors.TextEdit();
+			this.txtEditComposer = new DevExpress.XtraEditors.TextEdit();
+			this.btnClear = new DevExpress.XtraEditors.SimpleButton();
+			this.mvvmContext1 = new DevExpress.Utils.MVVM.MVVMContext(this.components);
+			this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+			this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+			((System.ComponentModel.ISupportInitialize)(this.txtEditId.Properties)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.txtEditName.Properties)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.txtEditComposer.Properties)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtEditId
+			// 
+			this.txtEditId.Location = new System.Drawing.Point(76, 99);
+			this.txtEditId.Name = "txtEditId";
+			this.txtEditId.Size = new System.Drawing.Size(232, 20);
+			this.txtEditId.TabIndex = 0;
+			// 
+			// txtEditName
+			// 
+			this.txtEditName.Location = new System.Drawing.Point(76, 125);
+			this.txtEditName.Name = "txtEditName";
+			this.txtEditName.Size = new System.Drawing.Size(232, 20);
+			this.txtEditName.TabIndex = 1;
+			// 
+			// txtEditComposer
+			// 
+			this.txtEditComposer.Location = new System.Drawing.Point(76, 152);
+			this.txtEditComposer.Name = "txtEditComposer";
+			this.txtEditComposer.Size = new System.Drawing.Size(232, 20);
+			this.txtEditComposer.TabIndex = 2;
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(76, 32);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 3;
+			this.btnClear.Text = "Clear Name";
+			// 
+			// mvvmContext1
+			// 
+			this.mvvmContext1.ContainerControl = this;
+			this.mvvmContext1.ViewModelType = typeof(Webinar.ViewModels.TrackViewModel);
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(197, 32);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 4;
+			this.btnSave.Text = "Save";
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(321, 31);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 5;
+			this.btnCancel.Text = "Cancel";
+			// 
+			// TrackView
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.txtEditComposer);
+			this.Controls.Add(this.txtEditName);
+			this.Controls.Add(this.txtEditId);
+			this.Name = "TrackView";
+			this.Size = new System.Drawing.Size(458, 422);
+			((System.ComponentModel.ISupportInitialize)(this.txtEditId.Properties)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.txtEditName.Properties)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.txtEditComposer.Properties)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private DevExpress.Utils.MVVM.MVVMContext mvvmContext1;
+        private DevExpress.XtraEditors.TextEdit txtEditComposer;
+        private DevExpress.XtraEditors.TextEdit txtEditName;
+        private DevExpress.XtraEditors.TextEdit txtEditId;
+        private DevExpress.XtraEditors.SimpleButton btnClear;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+    }
+}
---
+++ Webinar/Views/TrackView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using Webinar.ViewModels;
+
+namespace Webinar.Views
+{
+    public partial class TrackView : DevExpress.XtraEditors.XtraUserControl
+    {
+        public TrackView()
+        {
+            InitializeComponent();
+			
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (!DesignMode)
+                InitBindings();
+        }
+
+        void InitBindings()
+        {
+            var f = mvvmContext1.OfType<TrackViewModel>();
+
+            f.SetBinding(txtEditId, x => x.EditValue, x => x.TrackId);
+            f.SetBinding(txtEditName, x => x.EditValue, x => x.Name);
+            f.SetBinding(txtEditComposer, x => x.EditValue, x => x.Composer);
+            f.BindCommand(btnClear, x => x.ResetName());
+            f.BindCommand(btnSave, x => x.Save());
+            f.BindCommand(btnCancel, x => x.Revert());
+
+        }
+    }
+}
